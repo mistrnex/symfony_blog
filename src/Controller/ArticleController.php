2666200b69726f0399c55db6fd211edc93e635b7
mistrnex <38112062+mistrnex@users.noreply.github.com>
2020-08-30 22:22:16 +0200

    - beginning of CRUD

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -9,15 +11,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleController extends Controller {
     /**
-     * @Route("/")
+     * @Route("/", name="article_list")
      * @Method({"GET"})
      * @return Response
      */
     public function index () {
-//        return new Response('<html><body>Hello Ahojss Jenesssssssss 15566666dddd1</body></html>');
 
-        $articles = ['Article 1', 'Article 2'];
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+
+    public function view($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/view.html.twig', array('article' => $article));
+    }
+
+    // saving by get request
+
+//    /**
+//     * @Route("/article/save")
+//     */
+//    public function save() {
+//        $entityManager = $this->getDoctrine()->getmanager();
+//
+//        $article = new Article();
+//        $article->setTitle('Article Two');
+//        $article->setBody('Article two - Bla bla how are you?');
+//
+//        $entityManager->persist($article);
+//
+//        $entityManager->flush();
+//
+//        return new Response('Saved an article with the id of ' . $article->getId());
+//    }
 }
